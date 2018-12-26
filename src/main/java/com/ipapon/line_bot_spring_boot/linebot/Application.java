6f1e57aaf76e933901f745bdc05136de32a04a5b
punpapon <papon9@hotmail.com>
2018-12-26 15:31:18 +0700

    xxxx

--- src/main/java/com/ipapon/line_bot_spring_boot/linebot/Application.java
+++ src/main/java/com/ipapon/line_bot_spring_boot/linebot/Application.java
@@ -3,12 +3,28 @@ package com.ipapon.line_bot_spring_boot.linebot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 
 @SpringBootApplication
+@LineMessageHandler
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@EventMapping
+    public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+        System.out.println("event: " + e);
+        TextMessageContent message = e.getMessage();
+       
+        return new TextMessage(message.getText());
+    }
 
 }
