da60884a62ec252084a4e35a15d593c8fe6fc8fc
punpapon <papon9@hotmail.com>
2018-12-26 14:27:07 +0700

    test

---
+++ src/main/java/com/ipapon/line_bot_spring_boot/linebot/Application.java
@@ -0,0 +1,14 @@
+package com.ipapon.line_bot_spring_boot.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
