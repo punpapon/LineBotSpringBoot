18f1522e2b3897ac508c3bfc9a965046c5a27c1f
punpapon <papon9@hotmail.com>
2020-01-03 13:19:54 +0700

    Add existing file

--- src/main/java/com/ipapon/line_bot_spring_boot/linebot/Application.java
+++ src/main/java/com/ipapon/line_bot_spring_boot/linebot/Application.java
@@ -21,10 +21,75 @@ public class Application {
 	
 	@EventMapping
     public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
-        System.out.println("event: " + e);
-        TextMessageContent message = e.getMessage();
-       
-        return new TextMessage(message.getText());
+
+         log.info(event.toString());
+        TextMessageContent message = event.getMessage();
+        handleTextContent(event.getReplyToken(), event, message);
+
+        // System.out.println("event: " + e);
+        // TextMessageContent message = e.getMessage();
+        // return new TextMessage(message.getText());
     }
+
+
+      private void handleTextContent(String replyToken, Event event, 
+                                   TextMessageContent content) {
+        String text = content.getText();
+
+        log.info("Got text message from %s : %s", replyToken, text);
+
+        switch (text) {
+            case "Profile": {
+                String userId = event.getSource().getUserId();
+                if(userId != null) {
+                    lineMessagingClient.getProfile(userId)
+                            .whenComplete((profile, throwable) -> {
+                                if(throwable != null) {
+                                    this.replyText(replyToken, throwable.getMessage());
+                                    return;
+                                }
+                                this.reply(replyToken, Arrays.asList(
+                                        new TextMessage("Display name: " + 
+                                                        profile.getDisplayName()),
+                                        new TextMessage("Status message: " + 
+                                                        profile.getStatusMessage()),
+                                        new TextMessage("User ID: " + 
+                                                        profile.getUserId())
+                                ));
+                            });
+                }
+                break;
+            }
+            default:
+                log.info("Return echo message %s : %s", replyToken, text);
+                this.replyText(replyToken, text);
+        }
+    }
+
+    private void replyText(@NonNull  String replyToken, @NonNull String message) {
+        if(replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken is not empty");
+        }
+
+        if(message.length() > 1000) {
+            message = message.substring(0, 1000 - 2) + "...";
+        }
+        this.reply(replyToken, new TextMessage(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) {
+        reply(replyToken, Collections.singletonList(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        try {
+            BotApiResponse response = lineMessagingClient.replyMessage(
+                    new ReplyMessage(replyToken, messages)
+            ).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
